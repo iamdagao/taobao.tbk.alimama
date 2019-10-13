a64bce5bbea9f75b9069b2616300d8277c7362cd
lamlaogao <56520296+lamlaogao@users.noreply.github.com>
2019-10-14 05:05:00 +0800

    Create demo.php

---
+++ demo.php
@@ -0,0 +1,24 @@
+<?php
+namespace app\demo\controller;
+use cmf\controller\HomeBaseController;
+use tbk;
+class IndexController extends HomeBaseController
+{
+    public function index()
+    {
+       $tbk=new \tbk\tbk();
+		//tbk\tbk.php
+		//相关商品
+		$arr=array(
+			'method'=>'taobao.tbk.item.recommend.get',	//API
+			'fields'=>'num_iid,title,pict_url,small_images,reserve_price,zk_final_price,user_type,provcity,item_url',	//字段
+			'num_iid'=>'590030941756',	//商品ID
+			'count'=>40,	//返回数量，默认20，最大值40
+			'platform'=>2,	//链接形式：1：PC，2：无线，默认：１
+		);
+		$tbk->arr=$arr;
+		$r=$tbk->getapi ();
+		print_r($r);
+		
+    }
+}
